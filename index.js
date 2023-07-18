ec15af41408834f1c2a5fffa7d32b3126679fae2
Dulal-Hossain-ice <dulalhossainice09@gmail.com>
2023-07-18 15:02:14 +0600

    new added

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var country = 'Bangladesh';
+
+function name(){
+    console.log(country)
+
+}
+name();  //if var is inside function then it local variable and if it out side the function it called Global.
